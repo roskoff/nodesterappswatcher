84f6cb3a4234217b20829c393bf10f55b60776ce
Eliseo Ocampos <roskoff@gmail.com>
2012-06-17 05:22:17 -0400

    Styles and server status

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 // The root URL for the RESTful services
-var urlAPI = "http://nodester.com/apps";
+var urlAPI = "http://nodester.com";
 
 function getApps() {
     loadConfiguration();
@@ -8,7 +8,7 @@ function getApps() {
     var pass = $("#pass").val();
 	$.ajax({
 		type: 'GET',
-		url: urlAPI,
+		url: urlAPI + "/apps",
 		dataType: "json",
         "beforeSend": function(xhr) {
             xhr.setRequestHeader("Authorization", "Basic " + btoa(user + ":" + pass))
@@ -18,27 +18,70 @@ function getApps() {
 	});
 }
 
+function getServerStatus() {
+	console.log('getServerStatus');
+	$.ajax({
+		type: 'GET',
+		url: urlAPI + "/status",
+		dataType: "json",
+		success: renderStatus,
+        error: renderServerError
+	});
+}
+
 function renderList(data) {
 	// JAX-RS serializes an empty list as null, and a 'collection of one' as an object (not an 'array of one')
 	var list = data == null ? [] : (data instanceof Array ? data : [data]);
     console.log("List: " + list);
-    $("#appsList").empty();
+    var appsList = $("#appsList");
+    appsList.empty();
+    appsList.append(
+        '<div class="span12">' +
+            '<div class="row-fluid">' +
+                '<div class="span6"><h4>App Name</h4></div>' +
+                '<div class="span6"><h4>Status</h4></div>' +
+            '</div>' +
+        '</div>');
 	$.each(list, function(index, app) {
-        $("#appsList").append(
-            $("<li>").append("<a href=\"http://"+app.name+".nodester.com\" target=\"_blank\">"+app.name+"</a> - "+app.running+"</li>"));
+        appsList.append(
+            '<div class="span12">' +
+                '<div class="row-fluid">' +
+                    '<div class="span6"><a href="http://'+app.name+'.nodester.com" target="_blank">'+app.name+'</a></div>' +
+                    '<div class="span6"><img id="img_'+app.name+'" width="24" src="/img/rocket-running-'+app.running+'.png"></div>' +
+                '</div>' +
+            '</div>');
+        var status = app.running === 'true' ? "running! \\m/" : "down :(";
+        $("#img_"+app.name).tooltip({"title":"App is " + status, "placement":"right"});//, "content":"this is the content"});
         console.log(app.name+' - '+app.running);
 	});
+}
+
+function renderStatus(data) {
+    var serverStatus = $("#serverStatus");
+    var statusLabel = data.status === "up" ? "success" : "important";
+    serverStatus.empty();
+    serverStatus.append(
+        '<div class="well">' +
+            '<p><a href="http://nodester.com" target="_blank">Nodester</a> sever status is <span class="label label-'+statusLabel+'">'+data.status.toUpperCase()+'</span>.' +
+            ' There are <strong>'+data.appshosted+'</strong> apps hosted and <strong>'+data.appsrunning+'</strong> of them are running.' +
+        '</div>');
+}
+
+function renderServerError(xhr, err){
+    $("#serverStatus").empty();
 }
 
 function renderError(xhr, err){
     var appsList = $("#appsList");
     appsList.empty();
-    appsList.append("<p>Couldn't get any app :(</p>");
+    var div = '<div class="alert alert-error"><p>Couldn\'t get any app :(</p>';
     if (xhr.status === 401){
-        appsList.append("<p><span>You should check the configuration tab!<span></p>");
+        div += '<em>You should check the configuration tab!</em>';
     } else {
-        appsList.append("<p>Unable to connect with nodester.com</p>");
+        div += '<em>Unable to connect with nodester.com</em>';
     }
+    div += '</div>';
+    appsList.append(div);
     console.log(xhr.readyState);
     console.log(xhr.status);
     console.log(xhr.responseText);
@@ -59,4 +102,5 @@ function loadConfiguration() {
 function saveAndUpdate(){
     saveConfiguration();
     getApps();
+    getServerStatus();
 }
